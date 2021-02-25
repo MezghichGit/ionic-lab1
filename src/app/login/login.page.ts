6e8020c318567d0dfa0b5ffd0c634adb0abcf1b2
amine <amine.mezghich@ensi-uma.tn>
2021-02-25 12:04:04 +0100

    fonction auth finie

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  login: string;
+  pwd: string;
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   auth() {
-    console.log("Un click");
+    console.log(this.login+" "+this.pwd);
     //1- recupérer le login + password
     //2- redirection vers la page home
+    this.router.navigate(["/home"]);
   }
 }
